191c05072e66b3869d5830a5d89580f390618783
robsonAraujo <robsonaraujocarmo@hotmail.com>
2020-10-12 14:49:00 -0300

    adjust ts

--- src/components/Button/IButtonProps.ts
+++
@@ -1,8 +0,0 @@
-export interface IButtonProps {
-  primary?: boolean
-  secondary?: boolean
-  tertiary?: boolean
-  fullWidth?: boolean
-  text: string
-  cta?: boolean
-}
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,8 +1,6 @@
 import * as S from './styles'
 import { IButtonProps } from './IButtonProps'
 
-const Button = (props: IButtonProps) => (
-  <S.Wrapper {...props}>{props.children}</S.Wrapper>
-)
+const Button = (props) => <S.Wrapper {...props}>{props.children}</S.Wrapper>
 
 export default Button
