644e4daf3dcb9939c6b60fcd47342d5b7ce6c82c
robsonAraujo <robsonaraujocarmo@hotmail.com>
2020-10-12 13:26:21 -0300

    Created header

---
+++ src/pages/servicos.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+import Header from 'components/Header'
+
+export const Wrapper = styled.div`
+  padding-left: 74px;
+`
+export default function Servicos() {
+  return (
+    <Wrapper>
+      <Header />
+    </Wrapper>
+  )
+}
